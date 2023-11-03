f9b17cd4d05e34314b0eb3b6e29bc7569ff8ec1b
ShotaMatsushima <shota19890523.engineer@gmail.com>
2023-11-03 14:10:20 +0900

    create local server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world");
+import express from "express";
+import { Express, Request, Response } from "express";
+
+const app: Express = express();
+const PORT = 8080;
+
+app.get("/allTodos", (req: Request, res: Response) => {
+  return res.send("Hello World!");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}＼(^o^)／`));
