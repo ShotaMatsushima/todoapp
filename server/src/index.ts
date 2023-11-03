70714c00b29778a8a9e20fd05cc6d2f0bb55a316
ShotaMatsushima <shota19890523.engineer@gmail.com>
2023-11-03 15:11:59 +0900

    create post api

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,9 +7,22 @@ const prisma = new PrismaClient();
 const app: Express = express();
 const PORT = 8080;
 
+app.use(express.json());
+
 app.get("/allTodos", async (req: Request, res: Response) => {
   const allTodos = await prisma.todo.findMany();
   return res.json(allTodos);
+});
+
+app.post("/createTodo", async (req: Request, res: Response) => {
+  const { title, isCompleted } = req.body;
+  const createTodo = await prisma.todo.create({
+    data: {
+      title,
+      isCompleted,
+    },
+  });
+  return res.json(createTodo);
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}＼(^o^)／`));
