9b3a75f0191893f8e2b32a4d72f4e1b3322be30c
ShotaMatsushima <shota19890523.engineer@gmail.com>
2023-11-03 14:51:24 +0900

    change prisma api

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import { Express, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app: Express = express();
 const PORT = 8080;
 
-app.get("/allTodos", (req: Request, res: Response) => {
-  return res.send("Hello World!");
+app.get("/allTodos", async (req: Request, res: Response) => {
+  const allTodos = await prisma.todo.findMany();
+  return res.json(allTodos);
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}＼(^o^)／`));
